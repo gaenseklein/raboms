3608a393063c9217a444768bf60285f5d507ec8f
jonny <dekan@gaenseklein.net>
2019-12-29 17:46:53 +0100

    update to version 0.5

--- README.md
+++ README.md
@@ -10,15 +10,59 @@ Primero intentamos hasta donde podemos llegar asi no más con puro html y javasc
 
 # como instalar
 
-En la carpeta "releases" estan las ultimas versiones. Guarda la version ultima en tu compu, ábrelo y ya. 
+## linux / con filemanager
+Abre un terminal y ponga 
+```
+git clone https://github.com/gaenseklein/raboms
+```
+eso copia todo el projecto del repositorio a la carpeta "raboms" en tu directorio de home.
+
+Despues pongas 
+```
+sh raboms/install.sh
+```
+eso abre el script de instalación, que te copia una entrada a tu menu del desktop para abrir el programa directamente de alli y usar el filemanager para eligir tus archivos. 
+despues de eso puedes eligir si quieres abrir la configuración para configurar las carpetas en donde tiene que buscar el script de inicio del filemanager para buscar tus mp3 y ogg.
+
+## todos demas / sin filemanager
+En la carpeta "releases" estan las ultimas versiones. Guarda la version ultima en tu compu, ábrelo y ya lo puedes usar directamente. 
 la version más actual: [actual.tar.gz](releases/actual.tar.gz)
-último release: [raboms version 0.4.1](releases/0.4.1.zip)
+último release: [raboms version 0.5](releases/0.5.zip)
+
+# filemanager
+El camino de programar eso con javascript tiene una desventaja grande: Javascript - o mejor el navegador - no tiene accesso a tu sistema de archivos. Por eso en la version sin filemanager lo tienes que escribir la ruta completa a la carpeta donde esta el archivo que quieres usar para un boton. 
+El filemanager es la solución para tod@s usarios de linux, porque el script de inicio busca todos mp3 y ogg para el javascript y les pasa ese información. Asi en el navegador/javascript se puede eligir casi como lo estas acostumbrado y ya contiene toda la información.
+El problema de eso es que el proceso de inicio se puede tardar mucho. Si no configuras en la instalación se busca en tu carpeta "home" y en la carpeta "media" para discos externos. 
+Si yo inicio así no mas con mi disco externo de 2TB se tarda casi 2 minutos o más para arancar. Eso es normal, tambien que no te dice nada en ese tiempo cuando esta buscando. Hay que esperar. 
+El proceso del inicio se puede hacer más rapido cuando configuras el filemanager para que solo busca en rutas que de verdad tienen tus mp3s. Por ejemplo si tu musica esta en /home/usario/musica - eliges este carpeta para buscarlo. Si tus efectos estan en /home/usario/efectos añadis ese carpeta tambien. 
+
+## configurar el filemanager
+Se puede configurar de dos formas:
+1. con el script "config"
+2. editando el archivo "rutas.cfg" con un editor de texto como xed, pluma, leaf, nano o que te gusta. 
+
+### el script config
+Abre un terminal y ponga 
+```
+sh raboms/config.sh
+```
+Aqui puedes eligir si quieres borrar o añadir una nueva ruta. 
+Si eliges añadir una ruta, se abre una ventana de eligir archivos. En ese ventana eliges la carpeta que quieres asumar al proceso de busqueda y ya. 
+Si eliges borrar se abre una nueva ventana en que eliges la ruta que no quieres buscar más.
+Cuando estas listo puedes salir del script o eligier a iniciar raboms
+
+### archivo rutas.cfg
+Si quieres editar las rutas por tu mismo es facil igual. Solo abre el archivo rutas.cfg y ponga las carpetas que quieres usar por alli. Por cada carpeta pongas una linea. Si quieres buscar por ejemplo en /home/usuario/musica y /home/usuario/entrevistas el archivo parece asi:
+```
+/home/usuario/musica/
+/home/usuario/entrevistas/ 
+```
 
 # idioma/language
 
-Lo hacemos en dos idiomas - ingles como idioma internacional y castellano porque es el idioma principal de nuestra comunidad. Si quieres añadir otro idioma - bien venido :)
+Lo hacemos en dos idiomas - ingles como idioma internacional y castellano porque es el idioma principal de nuestra comunidad. Si quieres añadir otro idioma - bienvenido :)
 
-# Eligir salida
+# Eligir salida / placa de audio
 
 Aunque ya esta instalado parece dificil lograrlo. La documentación no esta claro por alli. 
 Lo que encontré que dicen es: 
---
+++ src/fileManager.js
@@ -0,0 +1,166 @@
+var fileManager = {
+  fs:[],
+  importFs:function(basepath, filestring){
+      var newdir = {
+        basepath:basepath,
+        subdirectorys:[],
+        dirlist:[],
+        files:[]
+      };
+      var fileLines = filestring.split("\n");
+      for(var x=0;x<fileLines.length;x++){
+        var actline = fileLines[x];
+        var posOfTab = actline.indexOf(" \t");
+        if(posOfTab==-1)continue; //do nothing on no tab found
+        var actsize = actline.substring(0,posOfTab);
+        if(actsize * 1 == actsize){
+          let ending = " Byte";
+          if(actsize>1024){
+            actsize = actsize / 1024;
+            ending = " KB";
+          }
+          if(actsize>1024){
+            actsize = actsize / 1024;
+            ending = " MB";
+          }
+          actsize = Math.floor(actsize*100)/100;
+          actsize += ending;
+        }
+        actline = actline.substring(actline.indexOf(basepath)+basepath.length);
+
+        var actl = actline.split("/");
+        var actfilename = actl.pop();
+        var actsource = basepath + actline;
+        var actdir = newdir;
+        for(var fx=0;fx<actl.length;fx++){
+          if(actdir.dirlist.indexOf(actl[fx])===-1){
+            let actnewdir = {
+              basepath:actdir.basepath+actl[fx]+"/",
+              subdirectorys:[],
+              dirlist:[],
+              files:[]};
+            actdir.subdirectorys.push(actnewdir);
+            actdir.dirlist.push(actl[fx]);
+            actdir = actnewdir;
+          }else{
+            actdir = actdir.subdirectorys[actdir.dirlist.indexOf(actl[fx])];
+          }
+        }//for(fx)
+        actdir.files.push({
+          filename:actfilename,
+          source:actsource,
+          size:actsize,
+          basepath:actdir.basepath
+        });
+      }//for(x)
+      this.fs.push(newdir);
+  },
+  getRootDirectoryOfPath:function(path){
+    var actdir;
+    for(var x=0;x<this.fs.length;x++){
+      if(path.indexOf(this.fs[x].basepath)===0)actdir=this.fs[x];
+    }
+    //console.log(this.fs[0].basepath);
+    return actdir;
+  },
+  getDirectoryOfPath:function(path){
+    var actdir = this.getRootDirectoryOfPath(path);
+    if(!actdir)return "error: "+path + " not found";
+    var patharr=path.substring(actdir.basepath.length).split("/");
+    while(patharr.length>0){
+      let actp=patharr.shift();
+      if(actdir.dirlist.indexOf(actp)>-1)
+        actdir=actdir.subdirectorys[actdir.dirlist.indexOf(actp)];
+    }//while
+    return actdir;
+  },
+  ls:function(directory){
+    var rdiv = document.createElement("div");
+    rdiv.classList.add("fileManagerLs");
+    var actpath = document.createElement("div");
+    actpath.classList.add("fileManagerLsPath");
+    var path = directory.basepath;
+    var rootpath = this.getRootDirectoryOfPath(directory.basepath).basepath;
+    var patharr = new Array();
+
+    var rootsdiv = document.createElement("div")
+    rootsdiv.classList.add("roots");
+    for(var px = 0;px<this.fs.length;px++){
+      var rb = document.createElement("button");
+      if(rootpath === this.fs[px].basepath)rb.classList.add("active");
+      rb.classList.add("rootButton");
+      rb.value = this.fs[px].basepath
+      rb.onclick=function(){fileManager.lsPath(this.value);};
+      rb.innerText=this.fs[px].basepath;
+      rootsdiv.appendChild(rb);
+    }
+    actpath.appendChild(rootsdiv);
+
+    //patharr.push(rootpath);
+    var actpp = path.indexOf("/",rootpath.length+1);
+    while(actpp>0){
+      patharr.push(path.substring(0,actpp+1));
+      actpp = path.indexOf("/",actpp+1);
+    }
+    console.log(patharr);
+    for(var x=0;x<patharr.length;x++){
+      var pb = document.createElement("button");
+      if(patharr[x]===rootpath)pb.classList.add("activeRoot");
+      pb.value = patharr[x];
+      pb.innerText = patharr[x].substring(patharr[x].lastIndexOf("/",patharr[x].length-2)+1);
+      pb.onclick=function(){fileManager.lsPath(this.value);};
+      actpath.appendChild(pb);
+    }
+    rdiv.appendChild(actpath);
+    var wrapper = document.createElement("div");
+    wrapper.classList.add("fileManagerLsWrapper");
+    var dir = document.createElement("ul");
+    dir.classList.add("fileManagerLsDir");
+    //directory.dirlist.sort(function(a,b){if(a.basepath < b.basepath)return 1; else return -1});
+    for(var x=0;x<directory.dirlist.length;x++){
+      var li = document.createElement("li");
+      var button = document.createElement("button");
+      button.innerText = "📁 | "+directory.dirlist[x];
+      button.value=directory.basepath+directory.dirlist[x];
+      button.onclick = function(){fileManager.lsPath(this.value)};
+      li.appendChild(button);
+      dir.appendChild(li);
+    }
+    wrapper.appendChild(dir);
+    var files = document.createElement("ul");
+    files.classList.add("fileManagerLsFiles");
+    for(var x=0;x<directory.files.length;x++){
+      var li=document.createElement("li");
+      var button = document.createElement("button");
+      button.innerText = "🔊 | "+directory.files[x].filename;
+      button.value = directory.files[x].source;
+      button.onclick=function(){fileManager.fileChosen(this.value)};
+      var size = document.createElement("span");
+      size.innerText = directory.files[x].size;
+      li.appendChild(button);
+      li.appendChild(size);
+      files.appendChild(li);
+    }
+    wrapper.appendChild(files);
+    rdiv.appendChild(wrapper);
+    var target = document.getElementById("fileManagerLsTarget");
+    target.innerHTML = "";
+    target.appendChild(rdiv);
+  },
+  lsPath:function(ppath){
+    path = ppath;
+    if(this.fs.length===0)return;
+    if(!path)path=this.lastUsedPath;
+    if(!path)path=this.fs[0].basepath;
+    var directory = this.getDirectoryOfPath(path);
+    this.ls(directory);
+    this.lastUsedPath = path;
+    document.getElementById("fileManagerDialog").classList.add("show");
+  },
+  fileChosen:function(fullpath){
+    let filename = fullpath.substring(fullpath.lastIndexOf("/"));
+    let basepath = fullpath.substring(0,fullpath.lastIndexOf("/")+1);
+    raBoms.finishFile(filename,basepath);
+    document.getElementById("fileManagerDialog").classList.remove("show");
+  }
+}
